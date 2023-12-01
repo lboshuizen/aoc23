d8b169d546a3a37d8b2ff5e4acf64b12f3954065
Leon Boshuizen <llc.boshuizen@gmail.com>
2023-12-01 13:59:41 +0100

    cleanup

--- Day1.fs
+++ Day1.fs
@@ -1,32 +1,20 @@
 module Day1
 
-let isDigit c = System.Char.IsDigit c
-let asInt (c:char) = int c - int '0'
-
-let parse = List.map(Seq.filter( isDigit ) >> Seq.map asInt >> Seq.toList)
-
-let firstLast = let num (x,y) = 10 * x + y         
-                function
-                | [x] -> num (x,x)
-                | [] -> 0
-                | x::xs -> num (x,List.last xs) 
-
 let first (s:string) (i:string) = s.IndexOf(i)
 let last (s:string) (i:string) = s.LastIndexOf(i)
 
 let substitutes = [("one","1"); ("two","2"); ("three","3"); ("four","4"); ("five","5"); ("six","6"); ("seven","7"); ("eight","8"); ("nine","9")]
 
-let replace index sel (s:string) = List.map (fun (on,r) -> (index s on,r) ) substitutes
-                                   |> List.filter (fun (x,_) -> x <> -1)
-                                   |> function
-                                      | [] -> s
-                                      | l -> l |> sel fst |> s.Insert
+let calibrate = both (Seq.find isDigit) (Seq.findBack isDigit) >> fun (cx,cy) -> 10 * (atoi cx) + (atoi cy)
 
-let part1 = parse >> List.map firstLast >> List.sum 
+let part1 = Seq.map calibrate >> Seq.sum 
 
-let part2 =
-    let replFst = replace first List.minBy
-    let replLst = replace last List.maxBy
-    List.map (replFst >> replLst) >> part1
+let replace index selBy (s:string) = List.map (fun (on,r) -> (index s on,r) ) substitutes
+                                     |> List.filter (fun (x,_) -> x <> -1)
+                                     |> function
+                                        | [] -> s
+                                        | l -> l |> selBy fst |> s.Insert
 
-let Solve = both part1 part2
+let part2 = Seq.map (replace first List.minBy >> replace last List.maxBy) >> part1
+
+let Solve : (string seq -> int * int)  = both part1 part2
--- Prelude.fs
+++ Prelude.fs
@@ -4,3 +4,6 @@ module Prelude
 let both f g x = (f x, g x)
  
 let foldl = Seq.fold
+
+let isDigit c = System.Char.IsDigit c
+let atoi (c:char) = int c - int '0'
