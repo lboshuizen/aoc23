13d4d7c8d6c1972b08ae9618701494426d9ee2bc
Leon Boshuizen <llc.boshuizen@gmail.com>
2023-12-01 12:51:01 +0100

    day1

--- Day1.fs
+++ Day1.fs
@@ -1,10 +1,32 @@
 module Day1
 
-let parse xs = xs
+let isDigit c = System.Char.IsDigit c
+let asInt (c:char) = int c - int '0'
 
-let part1 = id
+let parse = List.map(Seq.filter( isDigit ) >> Seq.map asInt >> Seq.toList)
 
-let part2 _ = None
+let firstLast = let num (x,y) = 10 * x + y         
+                function
+                | [x] -> num (x,x)
+                | [] -> 0
+                | x::xs -> num (x,List.last xs) 
 
-// string list -> ('a,'a)
-let Solve xs = xs |> parse |> both part1 part2
+let first (s:string) (i:string) = s.IndexOf(i)
+let last (s:string) (i:string) = s.LastIndexOf(i)
+
+let substitutes = [("one","1"); ("two","2"); ("three","3"); ("four","4"); ("five","5"); ("six","6"); ("seven","7"); ("eight","8"); ("nine","9")]
+
+let replace index sel (s:string) = List.map (fun (on,r) -> (index s on,r) ) substitutes
+                                   |> List.filter (fun (x,_) -> x <> -1)
+                                   |> function
+                                      | [] -> s
+                                      | l -> l |> sel fst |> s.Insert
+
+let part1 = parse >> List.map firstLast >> List.sum 
+
+let part2 =
+    let replFst = replace first List.minBy
+    let replLst = replace last List.maxBy
+    List.map (replFst >> replLst) >> part1
+
+let Solve = both part1 part2
--- Prelude.fs
+++ Prelude.fs
@@ -2,3 +2,5 @@
 module Prelude
 
 let both f g x = (f x, g x)
+ 
+let foldl = Seq.fold
