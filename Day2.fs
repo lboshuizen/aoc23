b4da077ae6173026b78b5d97b7bb1dca08806050
Leon Boshuizen <llc.boshuizen@gmail.com>
2023-12-02 08:56:51 +0100

    cleanup

--- Day2.fs
+++ Day2.fs
@@ -1,12 +1,10 @@
 module Day2
                               
-let line xs = let hand = parseRegex "\s(\d+)\s(\w+)" (fun a -> (int a[0],a[1]))
-              let g = splitOn ':' xs
-              let sec = g[1] |> splitOn ';' |> Array.map (splitOn ',' >> Array.map hand >> Seq.ofArray) |> Seq.concat
-              let gi = g[0] |> splitOn ' ' |> fun a -> int a[1]
-              (gi,sec)
- 
-let parse  = Seq.map line
+let parse xs = let hand = parseRegex "\s(\d+)\s(\w+)" (fun a -> (int a[0],a[1]))
+               let g = splitOn ':' xs
+               let gi = g[0] |> parseRegex "Game (\d+)" (fun a -> int a[0])
+               let sec = g[1] |> splitOn ';' |> Seq.map (splitOn ',' >> Seq.map hand) |> Seq.concat
+               (gi,sec)
 
 let part1 =
     let limit = Map.ofList [("red",12); ("green",13); ("blue",14)]
@@ -14,8 +12,8 @@ let part1 =
     Seq.filter (snd >> Seq.forall (fun (n,c) -> n <= limit[c])) >> Seq.sumBy fst
 
 let part2 =
-    let power = Seq.groupBy snd >> Seq.map (snd >> Seq.maxBy fst >> fst) >> foldl (fun a c -> a * c) 1
+    let power = Seq.groupBy snd >> Seq.map (snd >> Seq.maxBy fst >> fst) >> Seq.reduce (*)
 
     Seq.sumBy (snd >> power)
 
-let Solve : (string seq -> int * int) = parse >> both part1 part2
+let Solve : (string seq -> int * int) = Seq.map parse >> both part1 part2
