ddcef74cf66cf30183eb1379608bae2d1935e241
Leon Boshuizen <llc.boshuizen@gmail.com>
2023-12-09 13:30:08 +0100

    simplified

--- Day9.fs
+++ Day9.fs
@@ -2,15 +2,12 @@ module Day9
 
 let parse = Seq.map allInt
 
-let history ft fr xs =
-    let rec go xs a = let ds = xs |> Seq.pairwise |> Seq.map (fun (a,b) -> b-a)
-                      match Seq.forall ((=)0) ds with
-                      | true -> (ft xs) :: a
-                      | false -> go ds ((ft xs) :: a)
+let rec history xs = let ds = xs |> Seq.pairwise |> Seq.map (fun (a,b) -> b-a)
+                     match Seq.forall ((=)0) ds with
+                     | true -> Seq.last xs
+                     | false -> (Seq.last xs) + history ds
 
-    go xs [] |> Seq.reduce fr
-
-let part1 = Seq.sumBy (history Seq.last (+))
-let part2 = Seq.sumBy (history Seq.head (flip (-)))
+let part1 = Seq.sumBy history
+let part2 =  Seq.sumBy (Seq.rev >> history)
 
 let Solve (xs:string seq) = xs |> parse |> both part1 part2
